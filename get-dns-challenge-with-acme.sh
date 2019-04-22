93891ff14de9779675efcc8466e00162cbdf30b2
Brian King <1854985+inflatador@users.noreply.github.com>
2019-04-22 15:38:05 -0500

    better output for DNS challenge

--- get-dns-challenge-with-acme.sh
+++ get-dns-challenge-with-acme.sh
@@ -4,12 +4,8 @@
 # author: Brian King
 # copyright: 2019
 # license: Apache
-# making dir manually due to https://github.com/Neilpang/acme.sh/issues/2223
-mkdir -p /root/.acme.sh
-touch /root/.acme.sh/account.conf
 pkgs="git"
 yum -q -y install $pkgs > /dev/null 2>&1
 git clone https://github.com/Neilpang/acme.sh.git /root/acme.sh/ > /dev/null 2>&1
-cd /root/acme.sh
-./acme.sh --issue --dns -d de06.rackesc.net --yes-I-know-dns-manual-mode-enough-go-ahead-please 2>&1 | tee /tmp/results
-grep "TXT value" /tmp/results | awk -F\' '{print $2}'
+/bin/bash /root/acme.sh/acme.sh --home /root/.acme.sh --issue --dns -d $domain --yes-I-know-dns-manual-mode-enough-go-ahead-please >> /var/log/cert_issue.log
+grep "TXT value" /var/log/cert_issue.log | awk -F\' '{print $2}'
