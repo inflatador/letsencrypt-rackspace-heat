fa069985bfb10c7d548f49baf821b7930e975556
bria4081 <1854985+inflatador@users.noreply.github.com>
2020-03-21 14:52:28 -0500

    fixed unwanted newline issue

--- get-dns-challenge-with-acme.sh
+++ get-dns-challenge-with-acme.sh
@@ -8,10 +8,10 @@ pkgs="git"
 yum -q -y install $pkgs > /dev/null 2>&1
 git clone https://github.com/Neilpang/acme.sh.git /root/acme.sh/ > /dev/null 2>&1
 /bin/bash /root/acme.sh/acme.sh --home /root/.acme.sh --issue --dns -d $domain --yes-I-know-dns-manual-mode-enough-go-ahead-please >> /var/log/cert_issue.log
-<<<<<<< HEAD
 grep "TXT value" /var/log/cert_issue.log | awk -F\' '{print $2}'
 CT=$(grep "TXT value" /var/log/cert_issue.log | awk -F\' '{print $2}')
 printf %s "${CT}"
 =======
 grep "TXT value" /var/log/cert_issue.log | awk -F\' '{print $2}'
->>>>>>> better output for DNS challenge
+CT=$(grep "TXT value" /var/log/cert_issue.log | awk -F\' '{print $2}')
+printf %s "${CT}"
